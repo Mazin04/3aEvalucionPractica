f493dbd74b11c7d6c297b8d6dc10c015c7ef95f9
Dalia <ortizmartin.dalia@tiernoparla.es>
2023-05-27 18:12:42 +0200

    anadidas Dao que faltaban

--- src/es/tiernoparla/dam/galeria/model/DAOFactory.java
+++ src/es/tiernoparla/dam/galeria/model/DAOFactory.java
@@ -5,10 +5,12 @@ import java.sql.SQLException;
 public class DAOFactory {
     public static final int MODO_TEST = 0;
     public static final int MODO_SQLITE = 1;
+    public static final int MODO_XML = 2;
 
     public static GaleriaDAO getDao(int modo) throws SQLException{
         switch(modo){
             case MODO_SQLITE: return new SQLiteGaleriaDAO();
+            case MODO_XML: return new XMLGaleriaDAO();
             default: return new TestGaleriaDAO();
         }
     }
