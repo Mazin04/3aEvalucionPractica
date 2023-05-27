0f25c7b32fc5e4c9fc94c4a1adbd39743d8f0376
Dalia <ortizmartin.dalia@tiernoparla.es>
2023-05-27 20:39:12 +0200

    creacion de clase test para JUnit

---
+++ src/es/tiernoparla/dam/galeria/test/ObraTest.java
@@ -0,0 +1,10 @@
+package es.tiernoparla.dam.galeria.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ObraTest {
+
+    
+}
